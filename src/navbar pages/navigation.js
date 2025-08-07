04f909a4b9d3e4a11d7017c0a80f20fcf4881012
tara970 <tarajef120@gmail.com>
2025-08-07 20:13:16 +0330

    Responsive change for tablet and mobile screen sizes

--- src/navbar pages/navigation.js
+++ src/navbar pages/navigation.js
@@ -24,9 +24,9 @@ function Navigation() {
           d="M0,128L80,149.3C160,171,320,213,480,213.3C640,213,800,171,960,144C1120,117,1280,107,1360,101.3L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
-           <div className="daste">
-            <h3>دسته بندی ها</h3>
-           </div>
+      <div className="daste">
+        <h3>دسته بندی ها</h3>
+      </div>
       <nav className="nav">
         <Link to="/products/category/men's clothing">
           <img src={menClothes} alt="Men" />
@@ -45,7 +45,11 @@ function Navigation() {
           <span>Electronics</span>
         </Link>
       </nav>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="bottom">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="bottom"
+      >
         <path
           fill="#2b1700"
           fill-opacity="1"
